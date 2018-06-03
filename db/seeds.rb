b7088ac2112cdc58a9bf9953969077b695b62f6d
EikiKanaoka <kakakaeee@gmail.com>
2018-06-03 21:15:03 +0900

    seed.rb テスト

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+post = ['東京都', '東京都', '千葉県', '千葉県', '千葉県']
+city = ['港区', '新宿区', '野田市', '流山市', '']
+
+5.times do |i|
+  Map.create!(
+    address: post[i] + city[i]
+  )
+end
